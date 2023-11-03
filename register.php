730b83cdd617a52ef128b7704c5d41353d1411fd
kiranpranay <pranaybannu12@gmail.com>
2023-11-04 02:34:54 +0530

    make uid manual
    Signed-off-by: kiranpranay <pranaybannu12@gmail.com>

--- register.php
+++ register.php
@@ -3,6 +3,11 @@ require('includes/connection.php');
 
 // API endpoint for user registration
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['name']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['role_id']) && isset($_POST['city'])) {
+    if($_POST['user_id']){
+        $user_id = $_POST['user_id'];
+    }else{
+        $user_id = NULL;
+    }
     $name = $_POST['name'];
     $email = $_POST['email'];
     $mobile = $_POST['mobile'];
@@ -11,7 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['name']) && isset($_PO
     $city = $_POST['city'];
     $domain = $_POST['domain'];
 
-    $query = "INSERT INTO `Users` (user_id, name, email,mobile, password, role_id, city, domain) VALUES (NULL, '$name', '$email','$mobile', '$password', $role_id, '$city', '$domain')";
+    $query = "INSERT INTO `Users` (user_id,user_id, name, email,mobile, password, role_id, city, domain) VALUES ($user_id, '$name', '$email','$mobile', '$password', $role_id, '$city', '$domain')";
 
     if (mysqli_query($conn, $query)) {
         echo json_encode(array('message' => 'User registered successfully'));
